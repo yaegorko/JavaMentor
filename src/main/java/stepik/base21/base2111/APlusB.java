6cba38a84496716f2d6602487db5b448ff91bfd4
Egor Kuznetsov <yaegorko@gmail.com>
2018-05-09 18:35:39 +0400

    compare double wit Math.abs improve

--- src/main/java/stepik/base21/base2111/APlusB.java
+++ src/main/java/stepik/base21/base2111/APlusB.java
@@ -3,11 +3,6 @@ package stepik.base21.base2111;
 public class APlusB {
 
     public static boolean doubleExpression(double a, double b, double c) {
-        boolean result = false;
-
-        if (Math.abs(c - (a + b)) < 0.0001d) {
-            result = true;
-        }
-        return result;
+       return Math.abs(c - (a + b)) < 0.0001d;
     }
 }
