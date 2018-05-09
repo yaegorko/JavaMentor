5813312342dd545bc6504358e385d60b7eef5fa3
Egor Kuznetsov <yaegorko@gmail.com>
2018-05-09 18:33:43 +0400

    compare double

---
+++ src/main/java/stepik/base21/base2111/APlusB.java
@@ -0,0 +1,13 @@
+package stepik.base21.base2111;
+
+public class APlusB {
+
+    public static boolean doubleExpression(double a, double b, double c) {
+        boolean result = false;
+
+        if (c - (a + b) < 0.0001d) {
+            result = true;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/stepik/base21/base2111/APlusBTest.java
@@ -0,0 +1,16 @@
+package stepik.base21.base2111;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class APlusBTest {
+
+    @Test
+    public void test1() {
+        assertThat(APlusB.doubleExpression(0.1d, 0.2d, 0.3d), is(true));
+        ;
+    }
+
+}
