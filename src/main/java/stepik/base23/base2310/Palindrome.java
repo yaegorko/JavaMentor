3ba97444052915a2e47314d6658ec40a3e5c1692
yaegorko <yaegorko@gmail.com>
2018-05-11 14:10:24 +0400

    Palindrome complite

--- src/main/java/stepik/base23/base2310/Palindrome.java
+++ src/main/java/stepik/base23/base2310/Palindrome.java
@@ -2,8 +2,7 @@ package stepik.base23.base2310;
 
 public class Palindrome {
     public static boolean isPalindrome(String text) {
-
-
-        return false; // your implementation here
+        StringBuilder str = new StringBuilder(text.toLowerCase().replaceAll("[^a-zA-Z0-9]", ""));
+        return str.toString().equals(str.reverse().toString()); // your implementation here
     }
 }
---
+++ src/test/java/stepik/base23/base2310/PalindromeTest.java
@@ -0,0 +1,14 @@
+package stepik.base23.base2310;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PalindromeTest {
+
+    @Test
+    public void test1() {
+        assertThat(Palindrome.isPalindrome("Madam, I'm Adam!"), is(true));
+    }
+}
