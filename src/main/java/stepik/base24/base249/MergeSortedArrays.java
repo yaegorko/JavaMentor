f35a66582d40f1756773401944b1315092bac1ce
yaegorko <yaegorko@gmail.com>
2018-05-13 18:12:09 +0400

    MergedArray complite

---
+++ src/main/java/stepik/base24/base249/MergeSortedArrays.java
@@ -0,0 +1,24 @@
+package stepik.base24.base249;
+
+public class MergeSortedArrays {
+
+    public static int[] mergeArrays(int[] a1, int[] a2) {
+
+        int[] mergedArray = new int[a1.length + a2.length];
+        int i =0 , j = 0;
+
+        while (i + j  < mergedArray.length) {
+            if (i < a1.length && j < a2.length) {
+                mergedArray[i+j] = a1[i] < a2[j] ? a1[i++] : a2[j++];
+            } else if (i == a1.length) {
+                System.arraycopy(a2, j, mergedArray, i+j, a2.length - j);
+                break;
+            } else if (j == a2.length) {
+                System.arraycopy(a1, i, mergedArray, i+j, a1.length - i);
+                break;
+            }
+        }
+
+        return mergedArray;
+    }
+}
---
+++ src/test/java/stepik/base24/base249/MergeSortedArraysTest.java
@@ -0,0 +1,19 @@
+package stepik.base24.base249;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class MergeSortedArraysTest {
+
+    @Test
+    public void whenGetTwoArraysWithEqualLength() {
+        assertThat(MergeSortedArrays.mergeArrays(new int[] {0, 1, 2}, new int[] {-1, 3, 5}), is(new int[] {-1, 0, 1, 2, 3, 5}));
+    }
+
+    @Test
+    public void whenGetTwoArraysFromLesson() {
+        assertThat(MergeSortedArrays.mergeArrays(new int[] {0, 2, 2}, new int[] {1, 3}), is(new int[]  {0, 1, 2, 2, 3}));
+    }
+}
