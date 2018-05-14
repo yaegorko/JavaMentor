8e06b90c80ecbea83165dae41a19d7ec5406c5b4
yaegorko <yaegorko@gmail.com>
2018-05-14 09:14:18 +0400

    more tests

--- src/test/java/stepik/base24/base249/MergeSortedArraysTest.java
+++ src/test/java/stepik/base24/base249/MergeSortedArraysTest.java
@@ -16,4 +16,9 @@ public class MergeSortedArraysTest {
     public void whenGetTwoArraysFromLesson() {
         assertThat(MergeSortedArrays.mergeArrays(new int[] {0, 2, 2}, new int[] {1, 3}), is(new int[]  {0, 1, 2, 2, 3}));
     }
+
+    @Test
+    public void whenSecondArrayLargerThenFirst() {
+        assertThat(MergeSortedArrays.mergeArrays(new int[] {0, 2, 2}, new int[] {1, 3, 5, 5}), is(new int[]  {0, 1, 2, 2, 3, 5, 5}));
+    }
 }
