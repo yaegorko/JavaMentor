1aaf4e30c4059b283402055f1a67da3c8167a9f0
Egor Kuznetsov <yaegorko@gmail.com>
2018-06-21 00:39:37 +0400

    count words in stream v0.2

--- src/main/java/stepik/step6/stream/Main.java
+++ src/main/java/stepik/step6/stream/Main.java
@@ -3,30 +3,53 @@ package stepik.step6.stream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.stream.IntStream;
+import java.util.*;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.parseString(main.readFromSystemIn());
+    }
+
     public String readFromSystemIn() {
 
         String string;
-        StringBuilder builder = null;
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, UTF_8))) {
+        String result = "";
+        // StringBuilder builder = null;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, UTF_8))) {
             while ((string = reader.readLine()) != null) {
-                builder.append(string);
+                result += string;
             }
+            //string = builder.toString();
         } catch (NullPointerException | IOException e) {
-
+            e.printStackTrace();
         }
-        return builder.toString();
+
+
+        return result;
     }
 
     public void parseString(String string) {
-        IntStream stream = string.codePoints();
-      List<String> result = stream.map(s -> s.startWith()).toArray();
-        System.out.println(stream);
+
+        List<String> myList = new ArrayList<>(Arrays.asList(string.replaceAll("[^A-Za-zА-Яа-я0-9]+", " ").split(" ")));
+        Map<String, Integer> resultMap = myList.stream()
+                .map(String::toLowerCase)
+                .collect(HashMap::new,
+                        (map, word) -> {
+                            if (map.containsKey(word))
+                                map.put(word, map.get(word) + 1);
+                            else
+                                map.put(word, 1);
+                        },
+                        HashMap::putAll);
+        resultMap.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue()
+                        .reversed()
+                        .thenComparing(Map.Entry.comparingByKey()))
+                .limit(10)
+                .forEach(x -> System.out.println(x.toString().replaceAll("[^A-Za-zА-Яа-я)]+", "")));
     }
 }
--- src/test/java/stepik/step6/stream/MainTest.java
+++ src/test/java/stepik/step6/stream/MainTest.java
@@ -7,13 +7,24 @@ import static org.junit.Assert.*;
 public class MainTest {
 
     Main main = new Main();
+    String string = "Мама, мыла-мыла-мыла раму!";
+    String string2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+            "Sed sodales consectetur purus at faucibus. Donec mi quam, tempor vel ipsum non, " +
+            "faucibus suscipit massa. Morbi lacinia velit blandit tincidunt efficitur. " +
+            "Vestibulum eget metus imperdiet sapien laoreet faucibus. " +
+            "Nunc eget vehicula mauris, ac auctor lorem. Lorem ipsum dolor sit amet, " +
+            "consectetur adipiscing elit. Integer vel odio nec mi tempor dignissim.";
 
     @Test
     public void testStringToStream() {
-        String string = "Мама мыла-мыла-мыла раму!";
+        //String string = "Мама мыла-мыла-мыла раму!";
         main.parseString(string);
     }
 
+    @Test
+    public void testStream() {
+        main.parseString(string2);
+    }
 
 
 }
